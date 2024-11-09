079c26b86bd638fd92015080f4278916869f89ab
DEBORAH FUNMILOLA OLABOYE <deboraholaboye@gmail.com>
2024-11-09 18:30:36 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['techhubwebapps.onrender.com', 'http://127.0.0.1:8000/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
