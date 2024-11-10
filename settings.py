28f1920c931b12cbbd4079ce8333bd9db51ae4b9
DeborahOlaboye <deboraholaboye@gmail.com>
2024-11-10 12:20:47 +0100

    Updated allowed hosts

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['techhubwebapplication.onrender.com', 'http://127.0.0.1:8000/']
+ALLOWED_HOSTS = ['techhubwebapplication.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
